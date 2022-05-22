c3678edd3b667c1db706bc1f1bf6f49092fc4f04
iZabrodina <100682511+iZabrodina@users.noreply.github.com>
2022-05-22 19:33:42 +0300

    Add files via upload

---
+++ test/tests.cpp
@@ -0,0 +1,69 @@
+// Copyright 2021 GHA Test Team
+#include <gtest/gtest.h>
+
+#include "textgen.h"
+
+TEST(AllTests, Prefix) {
+    std::vector<std::string> text = {"hello", "again", "my", "friend"};
+
+    std::set<std::deque<std::string>> expected_prefixes = {
+        {"hello", "again"}, {"again", "my"}, {"my", "friend"}
+    };
+
+    text_generator tg{2};
+    tg.power_up(text);
+    auto base = tg.get_base();
+
+    std::set<std::deque<std::string>> actual_prefixes;
+    for (auto& x : base) {
+        actual_prefixes.insert(x.first);
+    }
+
+    ASSERT_EQ(expected_prefixes.size(), actual_prefixes.size());
+    for (auto& x : expected_prefixes) {
+        ASSERT_TRUE(actual_prefixes.find(x) != actual_prefixes.end());
+    }
+}
+
+TEST(AllTests, Suffix) {
+    std::vector<std::string> text = {"hello", "again", "my", "friend"};
+
+    text_generator tg{2};
+    tg.power_up(text);
+    auto base = tg.get_base();
+
+    std::deque<std::string> list {"my"};
+    auto iterator = base[std::deque<std::string>{"hello", "again"}].begin();
+    for(auto& x : list)
+    {
+        ASSERT_EQ(iterator->first, x);
+        ++iterator;
+    }
+}
+
+TEST(AllTests, SuffixMulti) {
+    std::vector<std::string> text = {"hello", "again", "my", "friend", "hello", "again", "darling"};
+
+    text_generator tg{2};
+    tg.power_up(text);
+    auto base = tg.get_base();
+
+    std::deque<std::string> list {"darling","my"};
+    auto iterator = base[std::deque<std::string>{"hello", "again"}].begin();
+    for(auto& x : list)
+    {
+        ASSERT_EQ(iterator->first, x);
+        ++iterator;
+    }
+}
+
+TEST(AllTests, TextGeneration) {
+    std::vector<std::string> text = {"hello", "again", "my", "friend", "hello", "again", "darling"};
+
+    text_generator tg{3};
+    tg.power_up(text);
+    std::string out = tg.generate_text(10,5);
+
+    // Last symbol will be "." because algorithm will use word "darling", which one is end of the sentence.
+    ASSERT_EQ(out[out.length()-1], '.');
+}
