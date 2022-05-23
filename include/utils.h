ec0e4424d8c8a5f295ea97a18b7b3a95df809ae8
iZabrodina <100682511+iZabrodina@users.noreply.github.com>
2022-05-23 20:53:02 +0300

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -4,11 +4,11 @@
 #include <string>
 
 template<typename stream_t>
-    std::vector<std::string> read_stream(const stream_t& stream) {
+std::vector<std::string> read_stream(stream_t& stream) {
     std::vector<std::string> result;
     std::string word = "";
     unsigned char ch = stream.get();
-    while (!stream.eof()) {
+    while(!stream.eof()) {
         if (ch > 191 && ch < 224) {
             ch += 32;
         }
@@ -17,7 +17,11 @@ template<typename stream_t>
         }
         if ((ch > 223 && ch <= 255) || (ch > 96 && ch <= 123)) {
             word += ch;
-        } else if (word.size() > 0) { result.push_back(word); word = ""; }
+        }
+        else if(word.size() > 0) {
+            result.push_back(word);
+            word = "";
+        }
         ch = stream.get();
     }
     if (word.length()) {
@@ -25,3 +29,4 @@ template<typename stream_t>
     }
     return result;
 }
+
